34a797d5372edf77779d1d7ee33a791003b1abc2
Tom Rosenback <tom@carputec.com>
2020-12-01 13:25:45 +0200

    Add timing

--- Day1/AdventOfCode-2020-1/AdventOfCode-2020-1/Program.cs
+++ Day1/AdventOfCode-2020-1/AdventOfCode-2020-1/Program.cs
@@ -11,23 +11,32 @@ namespace AdventOfCode_2020_1
         {
             Console.WriteLine("Advent of code 2020 day 1!");
 
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             var report = File.ReadAllText(AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.LastIndexOf(AppDomain.CurrentDomain.FriendlyName)) + inputFile)?.Split("\r\n").Select(x => Convert.ToInt32(x));
+            watch.Stop();
+            Console.WriteLine("Read input took " + watch.ElapsedMilliseconds + "ms");
 
+            watch.Restart();
             var result = (from x in report
                           from y in report
                           where y != x && x + y == 2020
                           select new { x, y, m = x * y }).First();
 
-            Console.WriteLine("Part 1: ");
+            watch.Stop();
+
+            Console.WriteLine("Part 1 calculation took: " + watch.ElapsedMilliseconds + "ms");
             Console.WriteLine(result);
 
+            watch.Restart();
             var result2 = (from x in report
                           from y in report
                           from z in report
                           where x != y && y != z && z != x && x + y + z == 2020
                           select new { x, y, z, m = x * y * z }).First();
 
-            Console.WriteLine("Part 2: ");
+            watch.Stop();
+
+            Console.WriteLine("Part 2 calculation took: " + watch.ElapsedMilliseconds + "ms");
             Console.WriteLine(result2);
         }
     }
