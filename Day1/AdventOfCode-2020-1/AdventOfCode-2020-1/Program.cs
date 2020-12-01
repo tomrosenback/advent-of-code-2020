97b3e85ee26da564bc2e3b7024556bf38cc09d3b
Tom Rosenback <tom@carputec.com>
2020-12-01 13:40:40 +0200

    Add alternative solution for performance comparison

--- Day1/AdventOfCode-2020-1/AdventOfCode-2020-1/Program.cs
+++ Day1/AdventOfCode-2020-1/AdventOfCode-2020-1/Program.cs
@@ -12,7 +12,11 @@ namespace AdventOfCode_2020_1
             Console.WriteLine("Advent of code 2020 day 1!");
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            var report = File.ReadAllText(AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.LastIndexOf(AppDomain.CurrentDomain.FriendlyName)) + inputFile)?.Split("\r\n").Select(x => Convert.ToInt32(x));
+            var report = File.ReadAllText(AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.LastIndexOf(AppDomain.CurrentDomain.FriendlyName)) + inputFile)?
+                .Split("\r\n")
+                .Select(x => Convert.ToInt32(x))
+                .OrderBy(x => x);
+
             watch.Stop();
             Console.WriteLine("Read input took " + watch.ElapsedMilliseconds + "ms");
 
@@ -29,15 +33,58 @@ namespace AdventOfCode_2020_1
 
             watch.Restart();
             var result2 = (from x in report
-                          from y in report
-                          from z in report
-                          where x != y && y != z && z != x && x + y + z == 2020
-                          select new { x, y, z, m = x * y * z }).First();
+                           from y in report
+                           from z in report
+                           where x != y && y != z && z != x && x + y + z == 2020
+                           select new { x, y, z, m = x * y * z }).First();
 
             watch.Stop();
 
             Console.WriteLine("Part 2 calculation took: " + watch.ElapsedMilliseconds + "ms");
             Console.WriteLine(result2);
+
+            watch.Restart();
+            var foundSolution = false;
+
+            foreach (var x in report)
+            {
+                foreach (var y in report)
+                {
+                    var pair = x + y;
+
+                    if (pair > 2020)
+                    {
+                        break;
+                    }
+
+                    foreach (var z in report)
+                    {
+                        var triple = pair + z;
+                        if (triple > 2020)
+                        {
+                            break;
+                        }
+                        else if (triple == 2020)
+                        {
+                            watch.Stop();
+                            Console.WriteLine("Part 2, alternative solution took: " + watch.ElapsedMilliseconds + "ms");
+                            Console.WriteLine("{0} + {1} + {2} = 2020, multiplied = {3}", x, y, z, x * y * z);
+                            foundSolution = true;
+                            break;
+                        }
+                    }
+
+                    if(foundSolution)
+                    {
+                        break;
+                    }
+                }
+
+                if(foundSolution)
+                {
+                    break;
+                }
+            }
         }
     }
 }
